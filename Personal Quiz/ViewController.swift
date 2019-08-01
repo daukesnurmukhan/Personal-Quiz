2c78d317c62be4a41c9e2a37770b21c4c74e6d66
Alexey Efimov <lex.efimov@gmail.com>
2019-08-01 22:22:42 +0500

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Alexey Efimov on 01/08/2019.
+//  Copyright © 2019 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
